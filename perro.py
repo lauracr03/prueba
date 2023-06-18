1c216898ba678b67d11d2f74002295212a1c286a
Laura Caballero <a21carula@iesgrancapitan.org>
2023-06-18 19:20:54 +0200

    patata

--- perro.py
+++ perro.py
@@ -1,18 +1,15 @@
 """
 Clase Perro.
-:date: 2023-05-22
 
-:autor: Jaime Rabasco
+Autor: Jaime Rabasco Ronda.
 """
-class Perro:
+import sphinx
 
-    def __init__(self) :
-        """ Creamos el constructor con el atributo/campo self.ladrar que inicializara con guau"""
-        self.ladrar = 'Guau'
+class Perro:
+    def __init__(self):
+        self.ladrar= 'guau'
 
     def ladrar(self):
-        """Esta función hace que nuestro 'perro' ladre, tiene que salir por pantalla guau"""
-        print(self.ladrar)
+        print(self.ladrar())
 
-"""Creamos el objeto perro y hacemos que ladre."""
-p = Perro()
+p = Perro();
